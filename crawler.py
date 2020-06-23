341a238898c5e0afc6060561f1fa85b5f960bb28
LYH-YF <63840506+LYH-YF@users.noreply.github.com>
2020-06-23 15:50:59 +0800

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,5 +1,5 @@
 import sys
-
+import json
 import mongo_client
 from config import *
 from crawler_task import Task
@@ -10,18 +10,27 @@ if __name__ == '__main__':
         每个线程使用一个账户进行查询
     """
     usage = "USAGE:python crawler.py PHANTOMJS_PATH"
-    if len(sys.argv) != 2:
+    ''' if len(sys.argv) != 2:
+        #print(len(sys.argv))
         print(usage)
-        exit(1)
-    phantomjs_path = sys.argv[1]
-    accounts = mongo_client.get_accounts()
-    criteria = {"class.class1": {"$nin": ["图形的性质", "图形的变换"]}, "type": SINGLE_CHOICE}
-    count = mongo_client.get_unresolved_url_count(criteria)
+        print(sys.argv[0])
+        exit(1) '''
+    phantomjs_path =r'C:\Users\74765\Desktop\phantomjs-2.1.1-windows\bin\phantomjs.exe' #sys.argv[0]
+    #accounts = mongo_client.get_accounts()
+    with open(r'keys\accounts.json','r',encoding='utf-8') as f:
+        accounts=json.load(f)
+    #criteria = {"class.class1": {"$nin": ["图形的性质", "图形的变换"]}, "type": SINGLE_CHOICE}
+    criteria = {"type": SINGLE_CHOICE}
+    if ANALYSIS_ONLY:
+        count=mongo_client.get_fetched_false_count(criteria)
+    else:
+        count = mongo_client.get_unresolved_url_count(criteria)
+    
     thread_nums = len(accounts)
     batch_size = int(count / thread_nums) + len(accounts)
     # Exclude: $nin, include: $in
     id = 0
-    for account in accounts:
-        t = Task(id, thread_nums, SINGLE_CHOICE, criteria, account, False, batch_size, phantomjs_path)
-        t.start()
-        id += 1
+    #for account in accounts:
+    t = Task(id, thread_nums, SINGLE_CHOICE, criteria, accounts[2], False, batch_size, phantomjs_path,ANALYSIS_ONLY)
+    t.start()
+    id += 1
